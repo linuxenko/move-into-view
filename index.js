4c83a520c5994e94d1c605fd254b72ad4b86b5ee
Svetlana Linuxenko <linuxenko@yahoo.com>
2017-03-26 22:28:00 +0300

    Identity of a parent nodes

--- index.js
+++ index.js
@@ -7,8 +7,60 @@
  * License MIT 2017 Svetlana Linuxenko
  */
 
-function MoveIntoView () {
+/**
+ * Finds element parent nodes
+ *
+ * @name parentsOf
+ * @function
+ * @access private
+ * @param {Element} target HTML Element
+ * @param {Function} isParent(element)
+ * @returns {Element} Parent node
+ */
+function _parentsOf (target, isParent) {
+  var parent = target.parentElement;
+  var wrapper = parent;
 
+  function hasParentAttributes (element) {
+    var CSS = window.getComputedStyle(element);
+    return ['overflow', 'overflowX', 'overflowY']
+      .some(function (attr) {
+        return CSS[attr] === 'hidden';
+      });
+  }
+
+  if (typeof isParent !== 'function') {
+    isParent = function () { return false; };
+  }
+
+  while (1) {
+    if (isParent(parent) || hasParentAttributes(parent)) {
+      return {
+        parent: parent,
+        wrapper: wrapper
+      };
+    }
+
+    wrapper = parent;
+    parent = parent.parentElement;
+
+    if (!parent || parent.tagName === 'BODY') {
+      return;
+    }
+  }
+}
+
+function MoveIntoView (target, options) {
+  options = options || {};
+
+  var parents = _parentsOf(target, options.isParent);
+
+  var view = {
+    wrapper: parents.wrapper,
+    parent: parents.parent
+  };
+
+  return view;
 }
 
 module.exports = MoveIntoView;
--- tests/element-setup.test.js
+++ tests/element-setup.test.js
@@ -5,19 +5,86 @@ var resetCSS = require('./css/reset');
 var CSSTate = require('csstate');
 var cst = new CSSTate();
 
-cst.rule(resetCSS);
+function vMock () {
+  var vParent = document.createElement('div');
+  vParent.classList.add('scrollable');
+  vParent.setAttribute('id', 'scrollable');
+  var vChild = document.createElement('div');
+  vChild.classList.add('wrapper');
+  vChild.setAttribute('id', 'wrapper');
+  vParent.appendChild(vChild);
+
+  for (var i = 0; i < 10; i++) {
+    var el = document.createElement('div');
+    el.classList.add('child');
+    vChild.appendChild(el);
+  }
+
+  return vParent;
+}
+
+var vMockCSS = {
+  '.scrollable': {
+    'position': 'relative',
+    'width': '120px',
+    'height': '90px',
+    'overflow': 'hidden'
+  },
+
+  '.wrapper': {
+    'white-space': 'nowrap',
+    'position': 'absolute'
+  },
+
+  '.child': {
+    'display': 'inline-block',
+    'width': '90px',
+    'height': '86px',
+    'margin': '0px 2px'
+  }
+};
 
 describe('Test element setup', function () {
+  var vParent = null;
+
   beforeEach(function () {
     cst.rule(resetCSS);
+    cst.rule(vMockCSS);
   });
 
   afterEach(function () {
     cst.exit();
   });
 
-  it('should be to be', function () {
+  before(function () {
+    vParent = vMock();
+    document.body.appendChild(vParent);
+  });
+
+  after(function () {
+    document.body.removeChild(vParent);
+  });
+
+  it('should setup mockup currectly', function () {
     expect(MoveIntoView).to.be.exists;
+
+    var scrollable = document.getElementById('scrollable');
+
+    expect(scrollable.getBoundingClientRect().height).to.be.equal(90);
+    expect(scrollable.getBoundingClientRect().width).to.be.equal(120);
+
+    var wrapper = document.getElementById('wrapper');
+
+    expect(wrapper.getBoundingClientRect().width).to.be.equal(940);
+    expect(wrapper.childNodes[1].getBoundingClientRect().left).to.be.equal(96);
+  });
+
+  it('should find out parent node', function () {
+    var wrapper = document.getElementById('wrapper');
+    var firstChild = wrapper.childNodes[0];
+
+    expect(MoveIntoView(firstChild).wrapper).to.be.equal(wrapper);
+    expect(MoveIntoView(firstChild).parent).to.be.equal(vParent);
   });
 });
 
